2fafdabf32552ec100a1dc579f9ee604adfce159
Tanmaynigade <nigadetanmay20@gmail.com>
2024-10-31 22:56:04 +0530

    1st phase 1.1

--- app.js
+++ app.js
@@ -1,11 +1,40 @@
 const express = require("express");
 const app = express();
 const mongoose = require('mongoose');
+const Listing = require("../Airbnb_Project/models/listing");
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+
+main().then(()=>{
+    console.log("connect to db");
+}).catch(err =>{
+    console.log(err);
+})
+
+async function main() {
+    await mongoose.connect(MONGO_URL);
+}
+
+
 
 app.get("/", (req, res)=>{
 
  res.send("hi, i am root");
 });
+
+app.get("/testListing", async (req, res) => {
+        let sampleListing = new Listing({
+            title: "My New Villa",
+            description: "By the beach",
+            price : 1200,
+            location: "Calangute, Goa",
+            country: "India",
+        });
+
+       await sampleListing.save();
+       console.log("sample was saved");
+       res.send("Successful testing");
+});
+
 
 app.listen(8080, ()=> {
     console.log("server is listening to port 8080")
---
+++ models/listing.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema({
+    title: {
+        type:String,
+        required: true,
+
+    },
+    
+    description: String,
+    image: {
+        type: String,
+        default:
+            "https://unsplash.com/photos/a-house-with-a-blue-front-door-and-a-brown-front-door-xaqsFfoEq3o";
+
+        set: (v) => v === "" ? "https://unsplash.com/photos/a-house-with-a-blue-front-door-and-a-brown-front-door-xaqsFfoEq3o":v,
+    },
+    price: Number,
+    location: String,
+    country:String,
+});
+
+const Listing = mongoose.model("Listing", listingSchema);
+module.exports = Listing;
