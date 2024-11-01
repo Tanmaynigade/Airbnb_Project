391c58cc6e13c6020e4f9eeb9d0ad27e13134566
Tanmaynigade <nigadetanmay20@gmail.com>
2024-11-01 18:21:03 +0530

    1st phase 1.2

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express();
 const mongoose = require('mongoose');
 const Listing = require("../Airbnb_Project/models/listing");
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+const path = require("path");
+const methodOverride = require("method-override");
 
 main().then(()=>{
     console.log("connect to db");
@@ -14,26 +16,86 @@ async function main() {
     await mongoose.connect(MONGO_URL);
 }
 
-
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({extended: true}));
+app.use(methodOveerride("_method"));
 
 app.get("/", (req, res)=>{
 
  res.send("hi, i am root");
 });
 
-app.get("/testListing", async (req, res) => {
-        let sampleListing = new Listing({
-            title: "My New Villa",
-            description: "By the beach",
-            price : 1200,
-            location: "Calangute, Goa",
-            country: "India",
-        });
+// Index Route
+app.get("/listings", async (req, res) => {
+ const allListings = await  Listing.find({});
+   res.render("../Airbnb_Project/Views/listing/index.ejs", {allListings});
+    });
 
-       await sampleListing.save();
-       console.log("sample was saved");
-       res.send("Successful testing");
+    //New Route
+
+    app.get("/listings/new", (req, res)=>{
+        res.render("listings/new.ejs");
+    })
+
+
+    // Show Route
+
+    app.get("/listings/:id", async (req, res) => {
+        let {id} = req.params;
+        const listing = await Listing.findById(id);
+        res.render("listings/show.ejs", {listing});
+    });
+   
+    //Create Route
+     
+    app.post("/listings", async(req, res)=>{
+        const newListing = new Listing(req.body.listing);
+            await newListing.save();
+          res.redirect("/listings"); 
+    });
+
+    
+// Edit Route
+
+app.get("/listings/:id/edit", async (req, res) =>{
+    let {id} = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listing/edit.ejs", {listing});
 });
+
+// Update route
+
+app.put("/listings/:id", async (req, res)=>{
+    let {id} = req.params;
+   await Listing.findByIdAndUpdate(id, {...req.body.listing});
+   res.redirect(`/listings/${id}`);
+});
+
+
+//Delete Route
+
+app.delete("/listings/:id", async (req, res) =>{
+    let {id} = req.params;
+   let deletedListing = await Listing.findByIdAndDelete(id);
+   console.log(deletedListing);
+   res.redirect("/listings");
+})
+ 
+
+// app.get("/testListing", async (req, res) => {
+//         let sampleListing = new Listing({
+//             title: "My New Villa",
+//             description: "By the beach",
+//             price : 1200,
+//             location: "Calangute, Goa",
+//             country: "India",
+//         });
+
+//        await sampleListing.save();
+//        console.log("sample was saved");
+//        res.send("Successful testing");
+// });
 
 
 app.listen(8080, ()=> {
--- models/listing.js
+++ models/listing.js
@@ -1,24 +1,49 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+
 
 const listingSchema = new Schema({
     title: {
-        type:String,
-        required: true,
-
+        type: String,
+        require: true,
     },
-    
     description: String,
     image: {
-        type: String,
-        default:
-            "https://unsplash.com/photos/a-house-with-a-blue-front-door-and-a-brown-front-door-xaqsFfoEq3o";
-
-        set: (v) => v === "" ? "https://unsplash.com/photos/a-house-with-a-blue-front-door-and-a-brown-front-door-xaqsFfoEq3o":v,
+        url: String,
+        filename: String,
     },
     price: Number,
     location: String,
-    country:String,
+    country: String,
+    reviews: [{
+        type: Schema.Types.ObjectId,
+        ref: "Review",
+    }],
+    owner : {
+        type: Schema.Types.ObjectId,
+        ref: "User"
+    },
+    geometry: {
+        type: {
+          type: String, // Don't do `{ location: { type: String } }`
+          enum: ['Point'], // 'location.type' must be 'Point'
+          required: true
+        },
+        coordinates: {
+          type: [Number],
+          required: true
+        }
+    },   
+    category: {
+        type: String,
+        enum: [""]
+    }                       
+});
+
+listingSchema.post("findOneAndDelete", async(listing) => {
+    if(listing) {
+        await Review.deleteMany({ _id: {$in: listing.reviews}});   
+    }
 });
 
 const Listing = mongoose.model("Listing", listingSchema);
